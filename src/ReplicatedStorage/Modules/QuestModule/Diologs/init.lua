7118be1199479b3154f0ffe57c3cc00ae98ea10a
Vladislav Elcheninov <133252067+vvladislovv@users.noreply.github.com>
2024-06-01 01:41:49 +0300

    1. Дописал оповищения 2. Начал писать квесты, сделал одну кнопку, меню(гуи) + камеры полу круг 3. Решить не большие зависания на кругу 4. До делать полностью меню 5. Сделать наведение и индексы 6. Саму логику разговора вынести в другие скрипты этой папки 7. Проверить на ошибки и баги

--- src/ReplicatedStorage/Modules/QuestModule/Diologs/init.lua
+++ src/ReplicatedStorage/Modules/QuestModule/Diologs/init.lua
@@ -1,3 +1,124 @@
 local Diologs = {}
 
+Diologs.QuesetDialog = {
+    ["Bread"] = {
+        Name = 'Bread',
+        Icon = "rbxassetid://16312164831",
+        NoQuset = {
+            "Hello, My quest Stop",
+            "Hello, My quest Stop2",
+            "Hello, My quest Stop3",
+            "Hello, My quest Stop4",
+            "Hello, My quest Stop5"
+        }, --! Если не стоят квесты или нет квестов 
+        QusetTable = {
+            [1] = {
+                Icon = "rbxassetid://16312164831",
+                NameQuset = "BreadOneQuest", -- ! название квеста
+                QuestNameTask = "",
+                Dialogs = { -- ! Тектс для диологов 
+                    NewQuset = { -- ! Новый квест и новый диолог
+                        QusetNPCDiologs1 = {
+                            "Hello bro!",
+                            "Hello bro1!",
+                            "Hello bro2!",
+                            "Hello bro13!",
+                        },
+                        PlayerDiologs = {
+                            "Player Hello NPC",
+                            "Player Hello NPC2",
+                            "Player Hello NPC3",
+                        },
+                        QusetNPCDiologs2 = {
+                            "Hello bro!2",
+                            "Hello bro1!2",
+                            "Hello bro2!2",
+                            "Hello bro13!2",
+                        },
+                    },
+
+                    OldQuset = { -- // Старый квест и квест не выполнился 
+                        QusetNPCDiologs1 = {
+                            "TEsetr!",
+                            "TEsetr2!",
+                            "TEsetr3!",
+                            "HTEsetr13!",
+                        },
+                        PlayerDiologs = {
+                            "asdfasdfasdf",
+                            "sdfgsdfhsdfh",
+                            "sdfgsdfgsdfgsdfg",
+                        },
+                        QusetNPCDiologs2 = {
+                            "sdfscvxcvbxcvbxcv",
+                            "vvvvvvvvv",
+                            "ccccccc",
+                            "xxxxxxxxx",
+                        },
+                    },
+
+                    Completed = { -- // квесть выполнили
+                        QusetNPCDiologs1 = {
+                            "Completed!",
+                            "Completed2!",
+                            "Completed3!",
+                            "Completed13!",
+                        },
+                        PlayerDiologs = {
+                            "Completed",
+                            "Completedf",
+                            "Completedfff",
+                        },
+                        QusetNPCDiologs2 = {
+                            "fddddddd",
+                            "ssssss",
+                            "aaaaaaaaaa",
+                            "cccsssss",
+                        },
+                    }
+
+                },
+
+                Task = { -- // Задание 
+                    {
+                        Type = "CollectCoin", -- ! Collect snalik
+                        StartAmt = 0,
+                        NeedAmt = 100,
+                    },
+
+                    {
+                        Type = "KillHilder", --! Kill Hilder
+                        StartAmt = 0,
+                        NeedAmt = 10,
+                    },
+                    {
+                        Type = "KillSeeker", -- ! Kill Seeker
+                        StartAmt = 0,
+                        NeedAmt = 10,
+                    },
+                    {
+                        Type = "CollectTokenMap", -- ! Collect token at map
+                        StartAmt = 0,
+                        NeedAmt = 100,
+                    },
+
+                    {
+                        Type = "TimeGame", --! Timer
+                        StartAmt = 0,
+                        NeedAmt = 3600,
+                    },
+                },
+
+                Rewards = { --// Награда за квесты 
+                    {
+                        Rewar = "BaseSettings",
+                        Type = "Sneliki",
+                        Amount = 1000
+                    }
+                }
+            }
+        }
+    }
+}
+
 return Diologs 
--- src/ReplicatedStorage/Modules/QuestModule/init.lua
+++ src/ReplicatedStorage/Modules/QuestModule/init.lua
@@ -1,21 +1,31 @@
+local Player = game.Players.LocalPlayer
+local PlayerGui = Player:WaitForChild('PlayerGui')
+local UIs = PlayerGui:WaitForChild('UIs')
+local PlayerScript = Player:WaitForChild("PlayerScripts")
 local SoundService = game:GetService("SoundService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
 local UserInputService = game:GetService("UserInputService")
 
-local TweenModule = require(ReplicatedStorage.Libary.TweenModule)
+local FolderCamers = workspace.Map.GameSettings.NPC.Camers
+local CamCurr = game.Workspace.CurrentCamera
 
+local TweenModule = require(ReplicatedStorage.Libary.TweenModule)
+local PlayerModule = require(PlayerScript:WaitForChild("PlayerModule"))
+local Controls = PlayerModule:GetControls()
+
+local CameraOpen = false
+local CamOriginal = nil
+local TweenCamers = nil
 local QuestModule = {}
 
 function QuestModule:CheckNPC(Button)
     if Button.QuestNPC.Value == "Snail" then
-        print('Snail')
-       -- ButtonCam = Button.ShopOBJ.Value
-        --ShopModule:ShopMini(Button)
-
+        QuestModule:StartSystems(Button.QuestNPC.Value)
     end
 end
 
-function QuestModule:StartModule(input) -- тут еще бесконечный цикл (Нужно придумать как цикл останавливать почить на дефорумах)
+function QuestModule:StartModule(input)
     for _, Button in next, workspace.Map.GameSettings.Button:GetChildren() do
         if Button.Name == "Quest" then
 
@@ -42,5 +52,125 @@ function QuestModule:StartModule(input) -- тут еще бесконечный 
         end
     end
 end
+
+function TweenGuiModule()
+    coroutine.wrap(function()
+        TweenService:Create(UIs.FrameBlackScreen, TweenInfo.new(1,Enum.EasingStyle.Quad,Enum.EasingDirection.In),{BackgroundTransparency = 0}):Play()
+        task.wait(9)
+        TweenService:Create(UIs.FrameBlackScreen, TweenInfo.new(1,Enum.EasingStyle.Quad,Enum.EasingDirection.In),{BackgroundTransparency = 1}):Play()
+    end)()
+end
+
+
+function CameraCirile(CamsFolder,CamersMax,CameraMin,Cam) -- Тут смотреть
+    while CameraOpen do
+        CameraMin += 1
+        if CameraMin > CamersMax then
+            TweenGuiModule()
+            task.wait(0.3)
+            CameraMin = CamersMax
+            CameraMin = 1
+        end
+        print(CameraMin)
+        print(CamersMax)
+        TweenCamers = TweenService:Create(Cam, TweenInfo.new(8, Enum.EasingStyle.Linear,Enum.EasingDirection.InOut), {CFrame = CamsFolder['Cam'..CameraMin].CFrame})
+        TweenCamers:Play()
+        task.wait(8)
+    end
+end
+
+function EnabledUIModuleGui()
+    local BlockFrame = UIs.FrameBlock
+    TweenService:Create(BlockFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.359, 0,0.899, 0)}):Play() 
+    local function GuiQusetFrame()
+        local QusetFrame = UIs.QuestFrame
+        TweenService:Create(QusetFrame.FrameCancel,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.287, 0,1.5, 0)}):Play() -- {0.287, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameName1,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.077, 0,-0.1, 0)}):Play() -- {0.077, 0},{0.338, 0}
+        TweenService:Create(QusetFrame.FrameName2,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.364, 0,1, 0)}):Play() -- {0.364, 0},{0.34, 0}
+        TweenService:Create(QusetFrame.FrameQuest,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.221, 0,1.5, 0)}):Play() -- {0.221, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameTallk,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.156, 0,1.5, 0)}):Play() -- {0.156, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.MSGFRAME,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.154, 0,-1, 0)}):Play() -- {0.154, 0},{0.132, 0}
+        TweenService:Create(QusetFrame.NPCFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(1.5, 0,0.175, 0)}):Play() -- {0.355, 0},{0.175, 0}
+        TweenService:Create(QusetFrame.PlayerFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(-1, 0,0.175, 0)}):Play() -- {0.067, 0},{0.175, 0}
+        QusetFrame.Visible = false
+    end
+
+    GuiQusetFrame()
+end
+
+function UIModuleGui()
+    local BlockFrame = UIs.FrameBlock
+    TweenService:Create(BlockFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.359, 0,1, 0)}):Play() -- уезжает вниз ({0.359, 0},{0.899, 0})
+
+    local TableSizeQuestFrame = {
+        StartCancel = UDim2.new(0.061, 0,0.036, 0),
+        EndCancel = UDim2.new(0.067, 0,0.04, 0)
+    }
+
+    local function GuiQusetFrame()
+        local QusetFrame = UIs.QuestFrame
+        QusetFrame.Visible = true
+        --[[TweenService:Create(QusetFrame.FrameCancel,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.287, 0,1.5, 0)}):Play() -- {0.287, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameName1,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.077, 0,-0.1, 0)}):Play() -- {0.077, 0},{0.338, 0}
+        TweenService:Create(QusetFrame.FrameName2,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.364, 0,1, 0)}):Play() -- {0.364, 0},{0.34, 0}
+        TweenService:Create(QusetFrame.FrameQuest,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.221, 0,1.5, 0)}):Play() -- {0.221, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameTallk,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.156, 0,1.5, 0)}):Play() -- {0.156, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.MSGFRAME,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.154, 0,-1, 0)}):Play() -- {0.154, 0},{0.132, 0}
+        TweenService:Create(QusetFrame.NPCFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(1.5, 0,0.175, 0)}):Play() -- {0.355, 0},{0.175, 0}
+        TweenService:Create(QusetFrame.PlayerFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(-1, 0,0.175, 0)}):Play() -- {0.067, 0},{0.175, 0}
+        ]]
+        TweenService:Create(QusetFrame.FrameCancel,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.317, 0,0.391, 0)}):Play() -- {0.287, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameName1,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.077, 0,0.338, 0)}):Play() -- {0.077, 0},{0.338, 0}
+        TweenService:Create(QusetFrame.FrameName2,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.364, 0,0.34, 0)}):Play() -- {0.364, 0},{0.34, 0}
+        TweenService:Create(QusetFrame.FrameQuest,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.221, 0,0.374, 0)}):Play() -- {0.221, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameTallk,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.156, 0,0.374, 0)}):Play() -- {0.156, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.MSGFRAME,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.154, 0,0.132, 0)}):Play() -- {0.154, 0},{0.132, 0}
+        TweenService:Create(QusetFrame.NPCFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.355, 0,0.175, 0)}):Play() -- {0.355, 0},{0.175, 0}
+        TweenService:Create(QusetFrame.PlayerFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.067, 0,0.175, 0)}):Play() -- {0.067, 0},{0.175, 0}
+    end
+
+    local function GuiMouseHover()
+        local QusetFrame = UIs.QuestFrame
+        QusetFrame.FrameCancel.Frame.Frame.TextButton.MouseEnter:Connect(function()
+            QusetFrame.FrameCancel:TweenSize(TableSizeQuestFrame.EndCancel, Enum.EasingDirection.Out, Enum.EasingStyle.Sine,.25,true)
+        end)
+        QusetFrame.FrameCancel.Frame.Frame.TextButton.MouseLeave:Connect(function()
+            QusetFrame.FrameCancel:TweenSize(TableSizeQuestFrame.StartCancel, Enum.EasingDirection.Out, Enum.EasingStyle.Sine,.25,true)
+        end)
+
+        QusetFrame.FrameCancel.Frame.Frame.TextButton.MouseButton1Click:Connect(function()
+            QuestModule:CloseSystems()
+        end)
+    end
+    GuiQusetFrame()
+    GuiMouseHover()
+end
+
+function QuestModule:CloseSystems()
+    CameraOpen = false
+    TweenService:Create(CamCurr, TweenInfo.new(1, Enum.EasingStyle.Linear,Enum.EasingDirection.InOut), {CFrame = CamOriginal}):Play()
+    task.wait(0.1)
+    CamCurr.CameraType = Enum.CameraType.Custom
+    Controls:Enable()
+    EnabledUIModuleGui()
+
+end
+
+function QuestModule:StartSystems(QuestNPC)
+    if not CameraOpen then
+        CameraOpen = true
+        local CamsFolder = FolderCamers[QuestNPC]
+        local CamersMax = #CamsFolder:GetChildren()
+        CamCurr.CameraType = Enum.CameraType.Scriptable
+        CamOriginal = CamCurr.CFrame
+        Controls:Disable()
+        TweenService:Create(CamCurr, TweenInfo.new(1, Enum.EasingStyle.Linear,Enum.EasingDirection.InOut), {CFrame = CamsFolder.Cam1.CFrame}):Play()
+        UIModuleGui()
+        task.wait(0.8)
+        local CameraMin = 1
+        CameraCirile(CamsFolder,CamersMax,CameraMin,CamCurr)
+    end
+end
+
 
 return QuestModule
