07fd4a482c5c398e9fdd08028c16291f169b7f68
Vladislav Elcheninov <133252067+vvladislovv@users.noreply.github.com>
2024-06-01 22:07:49 +0300

    QuestModule

---
+++ src/ReplicatedStorage/Modules/QuestModule/DiologModule/init.lua
@@ -0,0 +1,3 @@
+local DiologModule = {}
+
+return DiologModule
---
+++ src/ReplicatedStorage/Modules/QuestModule/UIQuset/init.lua
@@ -0,0 +1,100 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
+
+local Player = game.Players.LocalPlayer
+local PlayerGui = Player:WaitForChild('PlayerGui')
+local UIs = PlayerGui:WaitForChild('UIs')
+local UIQuset = {}
+
+function UIQuset:TweenGuiModule()
+    TweenService:Create(UIs.FrameBlackScreen, TweenInfo.new(1,Enum.EasingStyle.Quad,Enum.EasingDirection.In),{BackgroundTransparency = 0}):Play()
+    task.wait(9)
+    TweenService:Create(UIs.FrameBlackScreen, TweenInfo.new(1,Enum.EasingStyle.Quad,Enum.EasingDirection.In),{BackgroundTransparency = 1}):Play()
+end
+
+function UIQuset:BlackSreenOff() -- поменять
+    TweenService:Create(UIs.FrameBlackScreen, TweenInfo.new(1,Enum.EasingStyle.Quad,Enum.EasingDirection.In),{BackgroundTransparency = 1}):Play()
+end
+
+function UIQuset:EnabledUIModuleGui()
+    local BlockFrame = UIs.FrameBlock
+    TweenService:Create(BlockFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.359, 0,0.899, 0)}):Play() 
+    local function GuiQusetFrame()
+        local QusetFrame = UIs.QuestFrame
+        TweenService:Create(QusetFrame.FrameCancel,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.322, 0,1.5, 0)}):Play() -- {0.287, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameName1,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(-0.1, 0, 0.338, 0)}):Play() -- {0.077, 0},{0.338, 0}
+        TweenService:Create(QusetFrame.FrameName2,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.364, 0,1, 0)}):Play() -- {0.364, 0},{0.34, 0}
+        TweenService:Create(QusetFrame.FrameQuest,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.254, 0,1.5, 0)}):Play() -- {0.221, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameTallk,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.185, 0,1.5, 0)}):Play() -- {0.156, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.MSGFRAME,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.154, 0,-1, 0)}):Play() -- {0.154, 0},{0.132, 0}
+        TweenService:Create(QusetFrame.NPCFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(1.5, 0,0.175, 0)}):Play() -- {0.355, 0},{0.175, 0}
+        TweenService:Create(QusetFrame.PlayerFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(-1, 0,0.175, 0)}):Play() -- {0.067, 0},{0.175, 0}
+        QusetFrame.Visible = false
+    end
+
+    GuiQusetFrame()
+end
+
+function UIQuset:UIModuleGui()
+    local BlockFrame = UIs.FrameBlock
+    TweenService:Create(BlockFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.359, 0,1, 0)}):Play() -- уезжает вниз ({0.359, 0},{0.899, 0})
+
+    local TableSizeQuestFrame = {
+        StartCancel = UDim2.new(0.061, 0,0.036, 0),
+        EndCancel = UDim2.new(0.067, 0,0.04, 0)
+    }
+
+    local function GuiQusetFrame()
+        local QusetFrame = UIs.QuestFrame
+        QusetFrame.Visible = true
+        --[[TweenService:Create(QusetFrame.FrameCancel,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.287, 0,1.5, 0)}):Play() -- {0.287, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameName1,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.077, 0,-0.1, 0)}):Play() -- {0.077, 0},{0.338, 0}
+        TweenService:Create(QusetFrame.FrameName2,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.364, 0,1, 0)}):Play() -- {0.364, 0},{0.34, 0}
+        TweenService:Create(QusetFrame.FrameQuest,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.221, 0,1.5, 0)}):Play() -- {0.221, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameTallk,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.156, 0,1.5, 0)}):Play() -- {0.156, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.MSGFRAME,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.154, 0,-1, 0)}):Play() -- {0.154, 0},{0.132, 0}
+        TweenService:Create(QusetFrame.NPCFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(1.5, 0,0.175, 0)}):Play() -- {0.355, 0},{0.175, 0}
+        TweenService:Create(QusetFrame.PlayerFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(-1, 0,0.175, 0)}):Play() -- {0.067, 0},{0.175, 0}
+        ]]
+        TweenService:Create(QusetFrame.FrameCancel,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.322, 0,0.392, 0)}):Play() -- {0.287, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameName1,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.077, 0,0.338, 0)}):Play() -- {0.077, 0},{0.338, 0}
+        TweenService:Create(QusetFrame.FrameName2,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.364, 0,0.34, 0)}):Play() -- {0.364, 0},{0.34, 0}
+        TweenService:Create(QusetFrame.FrameQuest,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.254, 0,0.392, 0)}):Play() -- {0.221, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.FrameTallk,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.185, 0,0.392, 0)}):Play() -- {0.156, 0},{0.374, 0}
+        TweenService:Create(QusetFrame.MSGFRAME,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.154, 0,0.132, 0)}):Play() -- {0.154, 0},{0.132, 0}
+        TweenService:Create(QusetFrame.NPCFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.355, 0,0.175, 0)}):Play() -- {0.355, 0},{0.175, 0}
+        TweenService:Create(QusetFrame.PlayerFrame,TweenInfo.new(0.8,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut), {Position = UDim2.new(0.067, 0,0.175, 0)}):Play() -- {0.067, 0},{0.175, 0}
+    end
+
+    local function GuiMouseHover()
+        local QusetFrame = UIs.QuestFrame
+        QusetFrame.FrameCancel.Frame.Frame.TextButton.MouseEnter:Connect(function()
+            QusetFrame.FrameCancel:TweenSize(TableSizeQuestFrame.EndCancel, Enum.EasingDirection.Out, Enum.EasingStyle.Sine,.25,true)
+        end)
+        QusetFrame.FrameCancel.Frame.Frame.TextButton.MouseLeave:Connect(function()
+            QusetFrame.FrameCancel:TweenSize(TableSizeQuestFrame.StartCancel, Enum.EasingDirection.Out, Enum.EasingStyle.Sine,.25,true)
+        end)
+
+        QusetFrame.FrameQuest.Frame.Frame.TextButton.MouseEnter:Connect(function()
+            QusetFrame.FrameQuest:TweenSize(TableSizeQuestFrame.EndCancel, Enum.EasingDirection.Out, Enum.EasingStyle.Sine,.25,true)
+        end)
+        QusetFrame.FrameQuest.Frame.Frame.TextButton.MouseLeave:Connect(function()
+            QusetFrame.FrameQuest:TweenSize(TableSizeQuestFrame.StartCancel, Enum.EasingDirection.Out, Enum.EasingStyle.Sine,.25,true)
+        end)
+
+        QusetFrame.FrameTallk.Frame.Frame.TextButton.MouseEnter:Connect(function()
+            QusetFrame.FrameTallk:TweenSize(TableSizeQuestFrame.EndCancel, Enum.EasingDirection.Out, Enum.EasingStyle.Sine,.25,true)
+        end)
+        QusetFrame.FrameTallk.Frame.Frame.TextButton.MouseLeave:Connect(function()
+            QusetFrame.FrameTallk:TweenSize(TableSizeQuestFrame.StartCancel, Enum.EasingDirection.Out, Enum.EasingStyle.Sine,.25,true)
+        end)
+
+        QusetFrame.FrameCancel.Frame.Frame.TextButton.MouseButton1Click:Connect(function()
+            require(ReplicatedStorage.Modules.QuestModule):CloseSystems()
+        end)
+    end
+    GuiQusetFrame()
+    GuiMouseHover()
+end
+
+return UIQuset
